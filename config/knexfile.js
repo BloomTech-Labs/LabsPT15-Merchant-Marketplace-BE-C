c5dd03e0a01ce0e53508ed8b47dd766329ee7580
jasonrhemann <jrhemann@gmail.com>
2021-02-09 19:49:48 -0800

    fixed duplicates in knex.js file

--- config/knexfile.js
+++ config/knexfile.js
@@ -7,17 +7,7 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
-    // connection: {
-    //   host: process.env.DATABASE_HOST || 'localhost',
-    //   port: process.env.DATABASE_PORT || 3000,
-    //   database: process.env.DATABASE_DB ||'postgres',
-    //   user: process.env.DATABASE_USER || 'postgres',
-    //   // dotenv isn't playing nice the the password entry specifically
-    //   // replace generic password string with local instance password
-    //   password: "password"
-    // },
-    migrations: { directory: 'data/migrations' },
-    seeds: { directory: 'data/seeds' },
+
     pool: {
       min: 2,
       max: 10,
